562e44abb8d50eb3ff8ec3574e6639c235b7d25e
Ollie Relph <ollie@relph.me>
2020-07-05 17:06:49 +0100

    Strip app

---
+++ TestingA11yExample/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StatusBar, StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View></View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
